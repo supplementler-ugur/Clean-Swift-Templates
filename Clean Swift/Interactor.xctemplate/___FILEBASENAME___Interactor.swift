d0c7f6d45c915cf044f40a5078eefc1c4942deb2
supplementler-ugur <66255676+supplementler-ugur@users.noreply.github.com>
2020-07-08 14:41:17 +0300

    Update ___FILEBASENAME___Interactor.swift

--- Clean Swift/Interactor.xctemplate/___FILEBASENAME___Interactor.swift
+++ Clean Swift/Interactor.xctemplate/___FILEBASENAME___Interactor.swift
@@ -23,13 +23,12 @@ protocol ___VARIABLE_sceneName___DataStore {
 class ___VARIABLE_sceneName___Interactor: ___VARIABLE_sceneName___BusinessLogic, ___VARIABLE_sceneName___DataStore {
 
     var presenter: ___VARIABLE_sceneName___PresentationLogic?
-    var worker: ___VARIABLE_sceneName___Worker?
+    lazy var worker = ___VARIABLE_sceneName___Worker()
 
     // MARK: Do something
 
     func doSomething(request: ___VARIABLE_sceneName___.Something.Request) {
-        worker = ___VARIABLE_sceneName___Worker()
-        worker?.doSomeWork()
+        worker.doSomeWork()
 
         let response = ___VARIABLE_sceneName___.Something.Response()
         presenter?.presentSomething(response: response)
